54cf66fc5b8acbdb1e718e209a94af5eec98cad7
Juho Vepsalainen <bebraw@gmail.com>
2013-03-30 10:20:23 +0200

    Add presentation template
    Based on Google IO slideset. More than enough for this purpose.

---
+++ presentation/slide_config.js
@@ -0,0 +1,37 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Survive JavaScript',
+    subtitle: 'Strategies and Tricks',
+    eventTitle: 'AgileJkl 2013',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    //favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['agilejkl'] // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Juho Vepsäläinen',
+    //company: 'Job Title, Google',
+    //gplus: 'http://plus.google.com/1234567890',
+    twitter: '@bebraw',
+    www: 'http://www.nixtu.info',
+    github: 'http://github.com/bebraw'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
---
+++ presentation/slides.md
@@ -0,0 +1,65 @@
+
+title: Agenda
+class: big
+build_lists: true
+
+Things we'll cover (list should build):
+
+- Bullet1
+- Bullet2
+- Bullet3
+
+---
+
+title: 1990s
+class: nobackground fill
+
+![MTV](http://farm5.staticflickr.com/4065/4489637577_ce553503d0_z.jpg)
+
+<footer class="source">source: http://www.flickr.com/photos/44302262@N08/4489637577/</footer>
+
+---
+
+title: Big Title Slide
+class: title-slide
+
+---
+
+title: Code Example
+
+Media Queries are sweet:
+
+<pre class="prettyprint" data-lang="css">
+@media screen and (max-width: 640px) {
+  #sidebar { display: none; }
+}
+</pre>
+
+---
+
+title: Once more, with JavaScript
+
+<pre class="prettyprint" data-lang="javascript">
+function isSmall() {
+  return window.matchMedia("(min-device-width: ???)").matches;
+}
+
+function hasTouch() {
+  return Modernizr.touch;
+}
+
+function detectFormFactor() {
+  var device = DESKTOP;
+  if (hasTouch()) {
+    device = isSmall() ? PHONE : TABLET;
+  }
+  return device;
+}
+</pre>
+
+---
+
+title: Centered content
+content_class: flexbox vcenter
+
+This content should be centered!
