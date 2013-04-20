709c406db9c18d7c2ef6d6cdef26d0ca271ca999
Juho Vepsalainen <bebraw@gmail.com>
2013-04-20 19:36:05 +0300

    Eliminate alert

--- presentations/agilejkl2013/js/slide-controller.js
+++ presentations/agilejkl2013/js/slide-controller.js
@@ -73,7 +73,7 @@ SlideController.prototype.onMessage_ = function(e) {
   // from file:// though.
   // TODO: It would be dope if FF implemented location.origin!
   if (e.origin != ORIGIN_ && ORIGIN_.indexOf('file://') != 0) {
-    alert('Someone tried to postMessage from an unknown origin');
+    //alert('Someone tried to postMessage from an unknown origin');
     return;
   }
 
