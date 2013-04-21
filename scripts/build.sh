93ea0b853cf60e988d6ee41bb62c712867138b44
Juho Vepsalainen <bebraw@gmail.com>
2013-04-21 09:19:22 +0300

    Set up initial build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo "should build now"
+cd "$(dirname "$0")"
+cd ..
+
+# update content
+if [ -d "survive-js.wiki" ]; then
+    cd survive-js.wiki
+    git pull
+    cd ..
+else
+    git clone git@github.com:bebraw/survive-js.wiki.git
+fi
+
+# build now
+node_modules/ghw/bin/ghw -c site.json -i survive-js.wiki -o out -t templates -s
+cp -r templates/[!.]* out
