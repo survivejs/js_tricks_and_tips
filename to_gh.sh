a650695fc67b124455f4729b7b8e37117bdc8e7e
Juho Vepsalainen <bebraw@gmail.com>
2014-04-27 10:18:05 +0300

    Exclude mathjax and mixpanel from the build
    ga still wip (needs a fix).

--- to_gh.sh
+++ to_gh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gitbook build --plugins=ga --pluginsConfig=gitbook.json
+gitbook build --plugins=-mixpanel:-mathjax --pluginsConfig=gitbook.json
 cp -rf _book/* ../survive-js-gh/
 rm -rf _book
