66c718214474e4de6d51df013d43765b763517f9
Juho Vepsalainen <bebraw@gmail.com>
2014-04-28 10:13:43 +0300

    Include ga plugin to build

--- to_gh.sh
+++ to_gh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gitbook build --plugins=-mixpanel:-mathjax --pluginsConfig=gitbook.json
+gitbook build --plugins=ga,-mixpanel,-mathjax --pluginsConfig=gitbook.json
 cp -rf _book/* ../survive-js-gh/
 rm -rf _book
