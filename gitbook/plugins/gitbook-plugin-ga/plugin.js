c44fb12d5e0a3cd3b25d8dce7e80c7e09b82c0ef
Juho Vepsalainen <bebraw@gmail.com>
2015-01-15 11:55:04 +0200

    Update

--- gitbook/plugins/gitbook-plugin-ga/plugin.js
+++ gitbook/plugins/gitbook-plugin-ga/plugin.js
@@ -4,7 +4,7 @@ require(["gitbook"], function(gitbook) {
     });
 
     gitbook.events.bind("page.change", function() {
-        ga('send', 'pageview');
+        ga('send', 'pageview', window.location.pathname+window.location.search);
     });
 
     gitbook.events.bind("exercise.submit", function(e, data) {
